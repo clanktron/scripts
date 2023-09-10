c395c3ead041767d635e198689ef477e6aeb15f2
clayton <crdcastro5@gmail.com>
2023-09-09 17:48:37 -0700

    chore: remove git server related scripts
    - these now live in my git-server repo

--- newrepo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Create a new repo on an existing git server
-REPO="${1:?}"
-OWNER="${clanktron:-2}"
-repo_path=/home/git/"$OWNER"/"$REPO".git
-mkdir "$repo_path" 
-git init --bare "$repo_path" 
--- remotegitserver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-USER=${USER=?}
-# Create a new git remote on a git server
-ssh "$USER"@nas "sudo add user "
